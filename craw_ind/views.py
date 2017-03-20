818e1d4dcedf0903a71f6f825712c49012805436
dchennaraidu <d.chenna.raidu@gmail.com>
2017-03-20 22:22:39 +0530

    temporary search

--- craw_ind/views.py
+++ craw_ind/views.py
@@ -15,9 +15,28 @@ from django.template import Context, Template,loader
 from django.db import connection #for truncating
 # Create your views here.
 
-def search(request):
-    # hello
+def matching(formlist):
+    linkfreq = [0]*100
+    qlen = len(formlist)
+    for _ in range(qlen):
+        tempdbstring = keywordsdata.objects.filter(keyword=formlist[_])
+        tempdbstring = str(tempdbstring)
+        tempdbstring = tempdbstring.split("$")                                  #$ removal
 
+        newdbint = list()
+
+        for i in range(len(tempdbstring)):
+            newdbint.append(int(i))
+
+        for i in range(len(newdbint)):
+            linkfreq[int(newdbint[i])] += 1
+
+    linkfreq.sort(reverse=True)
+    return linkfreq
+
+
+
+def search(request):
     html = ""
     if request.GET:
         stopwords = [ 'i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', 'her', 'hers', 'herself', 'it', 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', 'should', 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', 'couldn', 'didn', 'doesn', 'hadn', 'hasn', 'haven', 'isn', 'ma', 'mightn', 'mustn', 'needn', 'shan', 'shouldn', 'wasn', 'weren', 'won', 'wouldn', 'download', 'attachment', 'refer', 'please', 'sd/-', 'i.e', 'ay', 'dear', 's.no']
@@ -25,7 +44,8 @@ def search(request):
         formlist = re.sub("[^\w]", " ", form).split()                           #spliting words
         formlist = filter(lambda x: x not in stopwords, formlist)               #removing stopwords
 
-        html = (formlist)
+        html = matching(formlist)
+
         # temp = keywordsdata.objects.filter(keyword="student")
     html += "<h1>Indevelopment</h1>"
     temp = keywordsdata.objects.filter(keyword="student")
