f04c6b329ae780d297ea3999a4b7b7ce00999666
dchennaraidu <d.chenna.raidu@gmail.com>
2017-03-20 21:45:07 +0530

    stopwords removal from query

--- craw_ind/views.py
+++ craw_ind/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from craw_ind.models import keywordsdata
 
 from bs4 import BeautifulSoup
-import requests,urllib3,os, string
+import requests,urllib3,os, string,re
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 
@@ -17,10 +17,16 @@ from django.db import connection #for truncating
 
 def search(request):
     # hello
+
     html = ""
     if request.GET:
-        form = (request.GET).get('searchquery')
-        html = form
+        stopwords = [ 'i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', 'her', 'hers', 'herself', 'it', 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', 'should', 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', 'couldn', 'didn', 'doesn', 'hadn', 'hasn', 'haven', 'isn', 'ma', 'mightn', 'mustn', 'needn', 'shan', 'shouldn', 'wasn', 'weren', 'won', 'wouldn', 'download', 'attachment', 'refer', 'please', 'sd/-', 'i.e', 'ay', 'dear', 's.no']
+        form = (request.GET).get('q')
+        formlist = re.sub("[^\w]", " ", form).split()                           #spliting words
+        formlist = filter(lambda x: x not in stopwords, formlist)               #removing stopwords
+
+        html = (formlist)
+        # temp = keywordsdata.objects.filter(keyword="student")
     html += "<h1>Indevelopment</h1>"
     temp = keywordsdata.objects.filter(keyword="student")
     temp = temp[0].location
